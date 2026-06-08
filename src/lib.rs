c9c477982a1f5120a1d145612056fe29003e8719
Philip Wales <me@ptwales.net>
2026-06-08 11:56:42 -0500

    use value_enum for colour

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ pub mod config;
 
 use config::Config;
 
+use clap::ValueEnum;
 use crossterm::cursor;
 use crossterm::event::{self, Event};
 use crossterm::style::{Color, Print, ResetColor, SetForegroundColor};
@@ -38,7 +39,7 @@ fn coin_flip() -> bool {
     RNG.with(|rng| (*rng).borrow_mut().random())
 }
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, ValueEnum)]
 pub enum MatrixColor {
     Black,
     Green,
